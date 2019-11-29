3a37edda977c7f19a5f04f40adb59783f83639f4
Luencer <luencer@163.com>
2019-11-29 13:29:09 +0800

    第一次提交

---
+++ src/Luenci/demo/fristDemo.java
@@ -0,0 +1,14 @@
+package Luenci.demo;
+
+/**
+ * @author Luenci
+ * @version 1.0
+ * @date 2019/11/29 13:12
+ */
+public class fristDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+}
