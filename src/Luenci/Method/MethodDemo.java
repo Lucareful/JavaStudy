30b672134d716ab24021c2374b25d62329d6f21b
Luencer <luencer@163.com>
2019-12-03 17:36:11 +0800

    Revert "第二次提交：基本数据类型"

---
+++ src/Luenci/Method/MethodDemo.java
@@ -0,0 +1,38 @@
+package Luenci.Method;
+
+/**
+ * @author Luenci
+ * @version 1.0
+ * @date 2019/12/3 17:02
+ */
+
+public class MethodDemo {
+    public static void main(String[] args){
+        farmer();
+
+
+    }
+
+    // 农民工作
+    public static void farmer(){
+        System.out.println("种地");
+        System.out.println("浇水");
+        System.out.println("收割");
+        System.out.println("出售");
+
+    }
+
+    // 商贩
+    public static void seller(){
+        System.out.println("采购");
+        System.out.println("打包");
+        System.out.println("上架");
+        System.out.println("售卖");
+    }
+
+    // 我
+    public static void me(){
+        System.out.println("吃");
+    }
+
+}
